f959eb60459387e27365d4644907f55c4cd719b8
User <User@DESKTOP-ENT97T7>
2021-03-01 22:46:42 +0200

    01.03.2020

--- src/practice.java
+++ src/practice.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class practice {
 
     public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        stringArrayList.add("Pazartesi");
+        stringArrayList.add("Salı");
+        stringArrayList.add("Çarşamba");
+        stringArrayList.add("Perşembe");
+        stringArrayList.add("Cuma");
+        stringArrayList.add("Cumartesi");
+        stringArrayList.add("Pazar");
+
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            System.out.println("Haftanın"+(i+1) +". inci günü: " +
+                    stringArrayList.get(i));
+        }
+        System.out.println("uzunluk: "+stringArrayList.size());
 
 
 
